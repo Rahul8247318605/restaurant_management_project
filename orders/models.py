d6ef040862a60aa2c98881d100163f9fe4a682a4
Rahul Y <yalagandularahul@gmail.com>
2025-09-25 15:00:15 +0530

    50819

--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,21 @@
 from django.db import models
-
-# Create your models here.
 [Imersive content redacted for brevity.]
+# Create your models here.
 from django.db import models
+class Order(models.Model):
+    status = model.ForeignKey('OrderStatus', on_delete=models.SET_NULL, null=True)
 
-class orderStatus(models.Model):
-    name = models.charField(max_length=50, unique=True)
+    def __str__(self):
+        return f"Order #{self.id}"
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.code
