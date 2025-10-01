cb5e1ea416bd61cb3c4273b316abdd0b58569096
Rahul Y <yalagandularahul@gmail.com>
2025-10-02 00:06:23 +0530

    52341

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from utils.validation_rules import validate_user_email
+from djando import forms
 # Create your views here.
 from rest_framework.generics import ListAPIView
 from .models import MenuCategory
 from .serializers import MenuCategorySerializer
 
-class MenuCategoryListAPIView(ListAPIView):
-    queryset = MenuCategory.objects.all()
-    serializer_class = MenuCategorySerializer
+class RegistrationForm(forms.Form):
+    email=forms.CharField(max_length=100)
+    password=forms.CharField(widget=forms.passwordInput)
+    def clean_email(self):
+        email_address=self.cleaned_data.get('email')
+        if not validate_user_email(email_address):
+            raise forms.ValidationError("Please enter a valid email address.")
+        return email_address
 
