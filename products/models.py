251bc61236aae46cac3231a9d7c7982713bd12c8
Rahul Y <yalagandularahul@gmail.com>
2025-10-01 23:56:44 +0530

    52342

--- products/models.py
+++ products/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    class Meta:
+        verbose_name_plural= "Categories"
+    def __str__(self):
+        return self.name
 class MenuItem(models.Model):
     name=models.CharField(max_length=255,unique=True,help_text="The name of the menu item.")
     description=models.TextField(blank=True, help_text="A short description of the item.")
