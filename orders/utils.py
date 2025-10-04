d0f1a8b5d5f15645b0cb20859136f20ca7911339
Rahul Y <yalagandularahul@gmail.com>
2025-10-04 22:49:17 +0530

    53050

--- orders/utils.py
+++ orders/utils.py
@@ -8,3 +8,7 @@ def generate_coupon_code(length=10):
         coupon_code = ''.join(secrets.choice(characters) for _ in) range(length))
         if not Coupon.objects.filter(code=coupon_code).exists():
             return coupon_code
+def get_daily_sales_total(specific_date:date):
+    orders=Order.objects.filter(created_at_date=specific_date)
+    total_sales=orders.aggregate(total_sum=Sum('total_price'))['total_sum']
+    return total_sales or 0
