973132c0909e4d54805e17b6f18cb2a9e625309f
Rahul Y <yalagandularahul@gmail.com>
2025-10-04 22:58:33 +0530

    53052

--- home/views.py
+++ home/views.py
@@ -5,6 +5,9 @@ from djando import forms
 from rest_framework.generics import ListAPIView
 from .models import MenuCategory
 from .serializers import MenuCategorySerializer
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
 
 class RegistrationForm(forms.Form):
     email=forms.CharField(max_length=100)
@@ -15,3 +18,6 @@ class RegistrationForm(forms.Form):
             raise forms.ValidationError("Please enter a valid email address.")
         return email_address
 
+class TableDetailView(generics.RetrieveAPIView):
+    queryset=Table.objects.all()
+    serializer_class=TableSerializer
