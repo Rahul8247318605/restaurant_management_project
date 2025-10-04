37bf871aca0314e043b3029991e3d1b3beaa85d8
Rahul Y <yalagandularahul@gmail.com>
2025-10-04 23:12:09 +0530

    53054

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 [Imersive content redacted for brevity.]
 # Create your models here.
+class Coupon(models.Model):
+    code=models.CharField(max_length=50,unique=True)
+    discount_percentage=models.DecimalField(max_digits=5,decimal_places=2)
+    is_active=models.BooleanField(default=True)
+    valid_from=models.DateField()
+    valid_until=models.DateField()
+    def __str__(self):
+        return self_code
 class ActiveOrderManager(models.Manager):
     def get_acttive_orders(self):
         return self.filter(status__in=['pending','processing'])
